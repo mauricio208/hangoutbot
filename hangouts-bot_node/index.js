8b0c4c2e1037d11656373e0830a58eb306c11818
Mauricio Machuca <mmachuca@cgtscorp.com>
2018-01-18 14:06:37 -0400

    node  bot

---
+++ hangouts-bot_node/index.js
@@ -0,0 +1,20 @@
+
+var hangoutsBot = require("hangouts-bot");
+var bot = new hangoutsBot(process.argv[2], process.argv[3]);
+
+bot.on('online', function() {
+	console.log('online');
+});
+
+bot.on('message', function(from, message) {
+	console.log(from + ">> " + message);
+
+	switch (message) {
+		case "help":
+			bot.sendMessage(from, "I an example Hangouts bot. Try saying hello.");
+			break;
+		case "hello":
+			bot.sendMessage(from, "Why hello to you too.");
+			break;
+	}
+});
